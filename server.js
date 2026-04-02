9829a905a35aeb340fa311a0a747ec9dbb1a3d54
alexei18 <aonea1828@gmail.com>
2026-04-02 20:02:42 +0300

    Increase multer file size limit to 100MB

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { htmlPagesToPdf } = require('./src/htmlToPdf');
 const app = express();
 const upload = multer({
   dest: 'uploads/',
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB
   fileFilter: (req, file, cb) => {
     if (file.mimetype !== 'application/pdf') {
       return cb(new Error('Only PDF files are allowed'));
